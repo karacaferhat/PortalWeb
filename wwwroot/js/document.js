c3dbf2c2305eaa17cf9995a53abb3cf32eca7e79
Mete ARSLAN <mete.arslan8@hotmail.com>
2021-07-18 21:23:54 +0300

    list function added

--- wwwroot/js/document.js
+++ wwwroot/js/document.js
@@ -1,10 +1,15 @@
 //!!!!ONEMLI!!!! azure dan cors u duzelt
-const baseUri = "https://tederikportaldocumentservice.azurewebsites.net/api/v1/deliverydocument/"
+const documentServiceBaseUri = "https://tederikportaldocumentservice.azurewebsites.net/api/v1/deliverydocument/";
+const loggerServiceBaseUri = "https://tedarikportallogger.azurewebsites.net/api/v1/log/";
+const blobStorageBaseUri = "https://tedarikportalstorage.blob.core.windows.net/";
 
 const status = document.getElementById("status");
 const selectButton = document.getElementById("select-button");
 const fileInput = document.getElementById("file-input");
-const processType = document.getElementById("process-type");
+const processTypeInput = document.getElementById("process-type");
+const listButton = document.getElementById("list-button");
+const fileList = document.getElementById("file-list");
+
 
 const uploadText = $("#upload-text");
 const uploadSpinner = $("#upload-spinner");
@@ -13,30 +18,18 @@ const uploadSpinner = $("#upload-spinner");
 const reportStatus = (message, isSpinning) => {
     status.innerHTML = `${message}<br/>`;
 
-    if(isSpinning){
+    if (isSpinning) {
         uploadText.css("display", "none");
         uploadSpinner.css("display", "block");
-    }
-    else{
+    } else {
         uploadText.css("display", "block");
         uploadSpinner.css("display", "none");
     }
 
 }
 
-
-const getServiceSasUriForContainerAndUpload = () => {
-    reportStatus("Uploading files...", true);
-
-
-    const request = {vendor : "701480",
-        processType: processType.value.toString(),
-        documentType: "asdfg",
-        asno: "123456789",
-        asLineNo: "123456"}
-
-
-    fetch(baseUri + "getServiceSasUriForContainer", {
+const fetchData = async (uri, request) => {
+    return await fetch( uri, {
         method: 'POST',
         mode: 'cors',
         headers: {
@@ -53,29 +46,60 @@ const getServiceSasUriForContainerAndUpload = () => {
         })
         .then(data => {
             if (data) {
-                uploadFiles(data.sasString);
+                return data;
             }
         })
         .catch(error => {
-            console.error('Couldn\'t Get SasString.' + error);
-            reportStatus('Couldn\'t Get SasString.' + error, false);
+            console.error('Couldn\'t Fetch. ' + error);
+            return null;
         });
-
 }
 
+const uploadFiles = async () => {
+    reportStatus("Uploading files...", true);
 
-const uploadFiles = async (sasString) => {
+    let vendor = "701480";
+    let processType = processTypeInput.value.toString();
+    let documentType = "asdfg";
+    let asno = "123456789";
+    let asLineNo = "123456";
+
+    let request = {
+        vendor: vendor,
+        processType: processType,
+        documentType: documentType,
+        asno: asno,
+        asLineNo: asLineNo
+    }
+
+    let sasString = (await fetchData(documentServiceBaseUri + "getServiceSasUriForContainer", request)).sasString;
+
     try {
+        if (!sasString)
+            throw Error("SasString is Null")
+
         const containerURL = new azblob.ContainerURL(sasString, azblob.StorageURL.newPipeline(new azblob.AnonymousCredential));
+
 
         const promises = [];
         for (const file of fileInput.files) {
+            if(await checkIfFileExists(processType, file.name, vendor, documentType, asno, asLineNo)) {
+                console.log("File Already Exists. It Won't Upload.");
+                reportStatus(`${file.name} Already Exists. Couldn't Upload`, false);
+                continue;
+            }
+
             const blockBlobURL = azblob.BlockBlobURL.fromContainerURL(containerURL, file.name);
+
             promises.push(azblob.uploadBrowserDataToBlockBlob(
                 azblob.Aborter.none, file, blockBlobURL));
+
+            let filePath = vendor + '/' + getFilePath(processType, file.name, vendor, documentType, asno, asLineNo);
+            await logUpload(vendor, asno, asLineNo, file.name, filePath);
         }
         await Promise.all(promises);
         reportStatus("Done.", false);
+        await listFiles();
 
     } catch (error) {
         console.log(error);
@@ -83,5 +107,78 @@ const uploadFiles = async (sasString) => {
     }
 }
 
+const listFiles = async () => {
+    const request = {
+        vendor: "701480",
+        fileType: "asdfg"
+    };
+
+    reportStatus("Listing Files...", false);
+    let data = await fetchData(documentServiceBaseUri + "getFilesByDocumentType", request);
+    reportStatus("Listed", false);
+
+
+    fileList.innerHTML = "";
+
+    data.forEach(f => {
+        fileList.innerHTML += `<a href="${blobStorageBaseUri + f.filepath}">
+            ${f.filename}
+        </a><br/>`;
+    });
+};
+
+const logUpload = async (vendor, asno, asLineNo, filename, fileurl) => {
+    let request = {
+        vendor: vendor,
+        asn: asno,
+        asnline: asLineNo,
+        filename : filename,
+        fileurl : fileurl,
+        resultType : "Success",
+        resultMessage : "Document has been succesfully uploaded."
+    };
+
+    let data = await fetchData(loggerServiceBaseUri + "addDocStoreLog", request);
+    console.log(data);
+}
+
+
+const getFilePath = (processType, fileName, vendor, documentType, asno, asLineNo) => {
+    let filePath = documentType + '/';
+    if (processType === "documentByType")
+        filePath += fileName;
+    else if (processType === "deliveryDocument")
+        filePath += asno + '/' + fileName;
+    else if (processType === "deliveryLineDocument")
+        filePath += asno + '/' + asLineNo + '/' + fileName;
+
+    return filePath;
+}
+
+const checkIfFileExists = async (processType, fileName, vendor, documentType, asno, asLineNo) => {
+    let filePath = getFilePath(processType, fileName, vendor, documentType, asno, asLineNo);
+
+    let request = {
+        containerName : vendor,
+        filePath : filePath
+    };
+
+    let data = false;
+    try {
+        data = await fetchData(documentServiceBaseUri + "checkIfFileExists", request);
+    }
+    catch (error){
+        return false
+    }
+
+    return data.result;
+}
+
+
+
 selectButton.addEventListener("click", () => fileInput.click());
-fileInput.addEventListener("change", getServiceSasUriForContainerAndUpload);
+fileInput.addEventListener("change", uploadFiles);
+
+listButton.addEventListener("click", listFiles);
+
+listFiles();
