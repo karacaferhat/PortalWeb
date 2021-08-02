903d9b4d2996e9b237b4594d8a59fa1b00a297e5
ferhat karaca <ferhat.karaca@gmail.com>
2021-08-02 12:25:39 +0300

    label update

--- wwwroot/js/printLabel.js
+++ wwwroot/js/printLabel.js
@@ -1,7 +1,7 @@
 $("#openPrintDialog").on("click", () => {
     JsBarcode("#barcode", "701480000156", {
-        width: 3,
-        height: 150
+        width: 4,
+        height: 100
     });
    $("#printDialog").modal("toggle");   
 });
