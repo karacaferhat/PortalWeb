d26c69dc28ee88bcde60a178518b471d4ec9f025
Mete ARSLAN <mete.arslan8@hotmail.com>
2021-08-04 12:53:38 +0300

    fix

--- wwwroot/js/delivery/preparingDeliveries.js
+++ wwwroot/js/delivery/preparingDeliveries.js
@@ -1,66 +1,87 @@
-let deliveryGridWithItems = new DeliveryGridWithItems('WAI', [{
-        dataField: "asn",
-        caption: "Sevkiyat No",
-        cellTemplate: function (container, options) {
-            $(`<a> ${options.value} </a>`)
-                .attr('href', "/Delivery/DeliveryProcess#" + options.value)
-                .appendTo(container);
+let deliveryGridWithItems = new DeliveryGridWithItems('WAI',
+    [{
+            dataField: "asn",
+            caption: "Sevkiyat No",
+            cellTemplate: function (container, options) {
+                $(`<a> ${options.value} </a>`)
+                    .attr('href', "/Delivery/DeliveryProcess#" + options.value)
+                    .appendTo(container);
+            }
+        },
+        {
+            dataField: "transportcompany",
+            caption: "Tasima Sirketi"
+        },
+        {
+            dataField: "transporttype",
+            caption: "Tasima Turu"
+        },
+        {
+            dataField: "issdate",
+            caption: "Sevkiyat Tarihi"
+        },
+        {
+            caption: "E-Irsaliye",
+            dataField: "edispatchfile",
+            cellTemplate: function (container, options) {
+                $(`<a> ${options.value.split('/').pop()} </a>`)
+                    .attr('href', blobStorageBaseUri + options.value)
+                    .attr('target', '_blank')
+                    .appendTo(container);
+            }
         }
-    },
-    {
-        dataField: "transportcompany",
-        caption: "Tasima Sirketi"
-    },
-    {
-        dataField: "transporttype",
-        caption: "Tasima Turu"
-    },
-    {
-        dataField: "issdate",
-        caption: "Sevkiyat Tarihi"
-    },
-    {
-        caption: "E-Irsaliye",
-        dataField: "edispatchfile",
-        cellTemplate: function (container, options) {
-            $(`<a> ${options.value.split('/').pop()} </a>`)
-                .attr('href', blobStorageBaseUri + options.value)
-                .attr('target', '_blank')
-                .appendTo(container);
+    ],
+    [{
+            dataField: "asn",
+            caption: "Sevkiyat"
+        },
+        {
+            dataField: "asnline",
+            caption: "Sevk Satır No"
+        },
+        {
+            dataField: "crdate",
+            caption: "Oluşturulma Tarihi"
+        },
+        {
+            dataField: "order",
+            caption: "Sipariş"
+        },
+        {
+            dataField: "sku",
+            caption: "Ürün Kodu"
+        },
+        {
+            dataField: "skuname",
+            caption: "Ürün Adı"
+        },
+        {
+            dataField: "ordqty",
+            caption: "Sipariş Miktari"
+        },
+        {
+            dataField: "ordunit",
+            caption: "Sipariş Birimi"
+        },
+        {
+            dataField: "dlvqty",
+            caption: "Sevk Miktari"
+        },
+        {
+            dataField: "dlvunit",
+            caption: "Sevk Birimi"
+        },
+        {
+            dataField: "lastdlvdate",
+            caption: "Son Gönderim Tarihi"
+        },
+        {
+            dataField: "package",
+            caption: "Paket No"
         }
-    }
-], [{
-        dataField: "sku",
-        caption: "SKU"
-    },
-    {
-        dataField: "skuname",
-        caption: "SKU Adi"
-    },
-    {
-        dataField: "ordqty",
-        caption: "Siparis Miktari"
-    },
-    {
-        dataField: "ordunit",
-        caption: "Siparis Miktar Birimi"
-    },
-    {
-        dataField: "dlvqty",
-        caption: "Sevkiyat Miktari"
-    },
-    {
-        dataField: "dlvunit",
-        caption: "Sevkiyat Miktari"
-    },
-    {
-        dataField: "revno",
-        caption: "Revizyon Numarasi"
-    }
-
-], {
-    selectionMode: 'single'
-});
+    ], {
+        selectionMode: 'single'
+    });
 
 const refreshGridButton = $("#refreshGridButton");
 
